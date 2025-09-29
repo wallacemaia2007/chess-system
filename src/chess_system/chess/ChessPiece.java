15584493551e76fe29d97d09350e56a61e0a94e2
Wallace Maia <wallacemaia2007@gmail.com>
2025-09-29 17:00:25 -0300

    Piece move count

--- src/chess_system/chess/ChessPiece.java
+++ src/chess_system/chess/ChessPiece.java
@@ -7,6 +7,7 @@ import chess_system.boardGame.Position;
 public abstract class ChessPiece extends Piece {
 
 	private Color color;
+	private int moveCount;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
@@ -24,9 +25,21 @@ public abstract class ChessPiece extends Piece {
 		return p != null && p.getColor() != color;
 
 	}
-	
+
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
+	}
+
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
+
+	public int getMoveCount() {
+		return moveCount;
 	}
 
 }
