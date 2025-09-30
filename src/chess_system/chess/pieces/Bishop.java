5d4fa75ce1667cec98121c4ae347e3b364a384dc
Wallace Maia <wallacemaia2007@gmail.com>
2025-09-30 16:24:24 -0300

    Bishop

---
+++ src/chess_system/chess/pieces/Bishop.java
@@ -0,0 +1,66 @@
+package chess_system.chess.pieces;
+
+import chess_system.boardGame.Board;
+import chess_system.boardGame.Position;
+import chess_system.chess.ChessPiece;
+import chess_system.chess.Color;
+
+public class Bishop extends ChessPiece {
+
+	public Bishop(Board board, Color color) {
+		super(board, color);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "B";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);
+
+		// nw
+		p.setValues(position.getRow() - 1, position.getColumn() - 1 );
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() - 1);;
+			
+		}
+		if (getBoard().positionExists(p) && isThereOponentPiece(p))
+			mat[p.getRow()][p.getColumn()] = true;
+		// ne
+		p.setValues(position.getRow() - 1, position.getColumn() + 1 );
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() + 1);;
+			
+		}
+		if (getBoard().positionExists(p) && isThereOponentPiece(p))
+			mat[p.getRow()][p.getColumn()] = true;
+		// se
+		p.setValues(position.getRow() + 1, position.getColumn() + 1 );
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() + 1);;
+			
+		}
+		if (getBoard().positionExists(p) && isThereOponentPiece(p))
+			mat[p.getRow()][p.getColumn()] = true;
+		// sw
+		p.setValues(position.getRow() + 1, position.getColumn() - 1 );
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() - 1);;
+			
+		}
+		if (getBoard().positionExists(p) && isThereOponentPiece(p))
+			mat[p.getRow()][p.getColumn()] = true;
+		
+		return mat;
+	}
+
+}
